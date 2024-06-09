9a4f749860bfcf69cd76ca006e6fe1120a57e076
Ayush <ayushsinghlts123@gmail.com>
2024-06-09 22:33:49 +0530

    cors

--- Notes_App_Backend/index.js
+++ Notes_App_Backend/index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'
+  }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
